92887f7d2d6e8b23e8ded87727e0c0f873b8646c
Vinay Mandge <vinay.mandge@capgemini.com>
2023-08-01 15:26:19 +0530

    LinkedInBot_0.1

--- src/main/java/ChromeBot/MainBot.java
+++ src/main/java/ChromeBot/MainBot.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,38 +25,17 @@ public class MainBot {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\VMANDGE\\Downloads\\chromedriver-win64\\chromedriver.exe");
 
         // Create a new instance of the ChromeDriver
-		 ChromeOptions options = new ChromeOptions();
-	      options.addArguments("user-data-dir=C:\\Users\\VMANDGE\\AppData\\Local\\Google\\Chrome\\User Data\\Default");
-	      options.addArguments("profile-directory=Profile 1");
-	      
-	        WebDriver driver = new ChromeDriver(options);
-	       // driver.get("https://www.linkedin.com");
-	
-	        WebDriverWait wait = new WebDriverWait(driver, 10);
-	        
-       
-//        WebElement emailInput = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("username")));
-//        emailInput.sendKeys("mandgevinay16@gmail.com");
-////        WebElement nextButton = driver.findElement(By.id("identifierNext"));
-////        nextButton.click();
-//        WebElement passwordInput = driver.findElement(By.id("password"));
-//        passwordInput.sendKeys("vkshimpi16599");
-//        WebElement signInButton = driver.findElement(By.xpath("//button[@type='submit']"));
-//        signInButton.click();
-//        
-//        WebElement searchBox = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("search-global-typeahead__input")));
-//        searchBox.sendKeys("Hiring Java developer");
-//        searchBox.sendKeys(Keys.ENTER);
-//        Thread.sleep(5000);
-//        List<WebElement> elements = driver.findElements(By.xpath("//*[contains(text(), '@')]"));
-//        System.out.println(elements.size());
-//        for (WebElement element : elements) {
-//        	System.out.println(element.isDisplayed());
-//        	
-//        }
-//        driver.close();
-        
-        Set<String> set = new HashSet<String>();
+		ChromeOptions options = new ChromeOptions();
+        options.addArguments("user-data-dir=C:\\Users\\VMANDGE\\AppData\\Local\\Google\\Chrome\\User Data\\Default");
+        options.addArguments("profile-directory=Profile 1");
+      
+        WebDriver driver = new ChromeDriver(options);
+      
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+        Set<String> Emailset = new HashSet<String>();
+        Set<String> URLset = new HashSet<String>();
         String str[] = {"Hiring Java developer","Looking Java developer","Looking for Java developer","Need Java developer","urgent Java developer","urgent Java developer","Java developer Immediate Joiner"};
         List<String> searchWords = new ArrayList<>(Arrays.asList(str));
         
@@ -64,38 +45,20 @@ public class MainBot {
         Pattern emailPattern = Pattern.compile("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}");
         
         for(String searchKey : searchWords) {
-        
         driver.get("https://www.linkedin.com/search/results/content/");
         WebElement searchBox = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("search-global-typeahead__input")));
         searchBox.sendKeys(searchKey);
         searchBox.sendKeys(Keys.ENTER);
 
-        // Wait for the search results to load
         try {
-            Thread.sleep(3000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 	
-	       
-	        long scrollTime = 10000; // 30 seconds
-	
-	     
-	        long startTime = System.currentTimeMillis();
-	
-	      
-	        while (System.currentTimeMillis() - startTime < scrollTime) {
-	           
-	            js.executeScript("window.scrollBy(0, 1000)");
-	
-	            
-	            try {
-	                Thread.sleep(500);
-	            } catch (InterruptedException e) {
-	                e.printStackTrace();
-	            }
-	        }
-
+	    //scroll down Page to get more results
+        scrollDownPage(js,20);
+        
         
         List<WebElement> posts = driver.findElements(By.className("break-words"));
         // Create a pattern to match email addresses
@@ -107,22 +70,58 @@ public class MainBot {
             Matcher matcher = emailPattern.matcher(descriptionText);
             while (matcher.find()) {
                 String email = matcher.group();
-                set.add(email);
-                System.out.println(email);
+                Emailset.add(email);
             	}
         	}
+        
+        List<WebElement> links = driver.findElements(By.xpath("//a[not(contains(@href, 'linkedin.com')) and not(starts-with(@href, '#'))]"));
+	        for (WebElement link : links) {
+	            String url = link.getAttribute("href");
+	            if(url.contains("feed") || url.contains("hashtag"))
+	            	continue;
+	            if (url.contains("mailto:")) {
+	            	url = url.replace("mailto:","");
+	                Emailset.add(url);
+	            } else {
+	                URLset.add(url);
+	            }
+	        }
         }
-        try (FileWriter writer = new FileWriter("output.txt");
+        String timeString = LocalDateTime.now().format(formatter);
+        
+        try (FileWriter writer = new FileWriter("EmailOutput_"+ timeString +".txt");
                 BufferedWriter bw = new BufferedWriter(writer)) {
-               for (String element : set) {
+               for (String element : Emailset) {
                    bw.write(element);
                    bw.newLine();
                }
            } catch (IOException e) {
                e.printStackTrace();
            }
-        driver.close();
+        try (FileWriter writer = new FileWriter("URLOutput_"+ timeString +".txt");
+                BufferedWriter bw = new BufferedWriter(writer)) {
+               for (String element : URLset) {
+                   bw.write(element);
+                   bw.newLine();
+               }
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+        
+     driver.close();
     }
+	
+	 private static void scrollDownPage(JavascriptExecutor js, int numScrolls) {
+	        for (int i = 0; i < numScrolls; i++) {
+	            js.executeScript("window.scrollBy(0, 1000)");
+
+	            try {
+	                Thread.sleep(500);
+	            } catch (InterruptedException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
 	
 }
 
