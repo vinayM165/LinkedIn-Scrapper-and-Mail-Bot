3971c1abf7545f29f01b97cf774f27a56b9758b2
Vinay Mandge <59912201+vinayM165@users.noreply.github.com>
2024-02-02 12:25:06 +0530

    Latest code changes

--- src/main/java/LinkedIn_Scrapper/MainBot.java
+++ src/main/java/LinkedIn_Scrapper/MainBot.java
@@ -3,7 +3,6 @@ package LinkedIn_Scrapper;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,7 +11,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
@@ -20,15 +19,16 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import Gmail_API.EmailMain;
-import LinkedIn_Scrapper.ChromOptionsConfig;
+
 public class MainBot {
 
 	public static void main(String args[]) throws Exception {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\VMANDGE\\Downloads\\chromedriver-win64\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp5pr\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
 
         // Create a new instance of the ChromeDriver
-		ChromOptionsConfig options = new ChromOptionsConfig(); 
-        WebDriver driver = new ChromeDriver(options.getConfig());
+		ChromeOptions options = new ChromeOptions();
+        options.addArguments("--user-data-dir=C:\\path\\to\\your\\custom\\directory");
+        WebDriver driver = new ChromeDriver(options);
 
         WebDriverWait wait = new WebDriverWait(driver, 10);
         
@@ -45,14 +45,16 @@ public class MainBot {
         Pattern emailPattern = Pattern.compile("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}");
        
         driver.get("https://www.linkedin.com/login");
+        if(isLoginPage(driver.getCurrentUrl())){
         WebElement emailInput = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("username")));
-        emailInput.sendKeys("Enter your email ID");
-        WebElement nextButton = driver.findElement(By.id("identifierNext"));
-        nextButton.click();
+        emailInput.sendKeys("mandgevinay16@gmail.com");
+        // WebElement nextButton = driver.findElement(By.id("identifierNext"));
+        // nextButton.click();
         WebElement passwordInput = driver.findElement(By.id("password"));
-        passwordInput.sendKeys("Enter password");
+        passwordInput.sendKeys("vkshimpi16599");
         WebElement signInButton = driver.findElement(By.xpath("//button[@type='submit']"));
         signInButton.click();
+        }
 
         for(String searchKey : searchWords) {
         driver.get("https://www.linkedin.com/search/results/content/");
@@ -67,7 +69,7 @@ public class MainBot {
 
         //scroll down Page to get more results
         //more value of 2nd parameter more data
-        scrollDownPage(js,60);
+        scrollDownPage(js,1);
 
 
         List<WebElement> posts = driver.findElements(By.className("break-words"));
@@ -121,20 +123,24 @@ public class MainBot {
 
      //starting to send mails
      EmailMain main = new EmailMain();
-     main.startEmailProcess();
+     //main.startEmailProcess();
     }
 	
 	 private static void scrollDownPage(JavascriptExecutor js, int numScrolls) {
-	        for (int i = 0; i < numScrolls; i++) {
-	            js.executeScript("window.scrollBy(0, 1000)");
+        for (int i = 0; i < numScrolls; i++) {
+            js.executeScript("window.scrollBy(0, 200)");
 
-	            try {
-	                Thread.sleep(500);
-	            } catch (InterruptedException e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private static boolean isLoginPage(String currentUrl) {
+        // You need to adjust this condition based on the actual login page URL
+        return currentUrl.contains("linkedin.com/login");
+    }
 	
 }
 
